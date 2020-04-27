1b036b8c9371b6f9b67c1f09a12801d37c58b249
ralph jularbal <ralphjularbal@yahoo.com>
2020-04-28 06:40:15 +0800

    Added signup with email and password functionailty

--- src/pages/login-registration-page/login-registration.component.jsx
+++ src/pages/login-registration-page/login-registration.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/signUp/signUp.component';
 
 import './login-registration.styles.scss';
 
 const LoginRegistrationPage = () => (
     <div className="sign-in-and-sign-up">
         <SignIn />
+        <SignUp />
     </div>
 );
 
