40ff2207cd03b9dec007bbf7795a9e9d7390ca20
Ralph Jularbal <ralphjularbal@yahoo.com>
2020-04-30 23:53:55 +0800

    Added checkout page, and checkout item. updated cart to use selectors from reselect

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,6 +7,10 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
+import { createStructuredSelector } from 'reselect';
+import { selectCurrentUser } from '../../redux/user/user.selector';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+
 const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link className='logo-container' to="/">
@@ -23,9 +27,9 @@ const Header = ({currentUser, hidden}) => (
             </Link>
             {
                 currentUser ?
-                <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                (<div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>)
                 :
-                <Link className='option' to='/signin'>SIGN IN</Link>
+                (<Link className='option' to='/signin'>SIGN IN</Link>)
             }
             <CartIcon />
         </div>
@@ -33,8 +37,9 @@ const Header = ({currentUser, hidden}) => (
     </div>
 )
 
-const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
-    currentUser, hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 });
 
 export default connect(mapStateToProps)(Header);
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -4,7 +4,7 @@ import { createSelector } from 'reselect';
 const selectCart = state => state.cart;
 
 //first param is the input selector/collection,
-//second param is a function that returns  the desired output of the input selector/collection
+//second param is a function that returns the desired output of the input selector/collection
 export const selectCartItems = createSelector( // <-- Memoization Selector
     [selectCart],
     (cart) => cart.cartItems
@@ -15,6 +15,20 @@ export const selectCartItemsCount = createSelector(
     cartItems =>
         cartItems.reduce(
             (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+            0
+        )
+);
+
+export const selectCartHidden = createSelector(
+    [selectCart],
+    cart => cart.hidden
+);
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems =>
+        cartItems.reduce(
+            (accumulatedQuantity, cartItem) => accumulatedQuantity + (cartItem.quantity * cartItem.price),
             0
         )
 )
