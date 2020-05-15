d6ec6025ae76c0c07c4aab6bbeb61e6e00014a93
Ralph Jularbal <ralphjularbal@yahoo.com>
2020-05-16 02:52:11 +0800

    Continuation for firebase promise based

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -23,12 +23,15 @@ class ShopPage extends React.Component {
   componentDidMount() {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
-
-    collectionRef.onSnapshot(async snapshot => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      updateCollections(collectionsMap);
-      this.setState({ loading : false });
-    });
+    fetch('https://firestore.googleapis.com/v1/projects/prmtv-clothing/databases/(default)/documents/collections')
+    .then(response =>  response.json())
+    .then(collections => console.log(collections));
+    
+    // collectionRef.onSnapshot(async snapshot => {
+    //   const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //   updateCollections(collectionsMap);
+    //   this.setState({ loading : false });
+    // });
   }
 
   render() {
